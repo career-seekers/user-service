e40432f4ca5932800e18a17832b19bb5706d645c
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-05-29 02:37:51 +0300

    build(core): init project

---
+++ build.gradle.kts
@@ -0,0 +1,108 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("kapt") version "1.9.0"
+    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.serialization") version "2.1.20"
+    id("org.springframework.boot") version "3.5.0"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("it.nicolasfarabegoli.conventional-commits") version "3.1.3"
+}
+
+group = "org.careerseekers"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // Spring security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    // JWT Auth
+    implementation("io.jsonwebtoken:jjwt:0.12.6")
+    implementation("javax.xml.bind:jaxb-api:2.3.0")
+
+    // Databases
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-database-postgresql")
+    implementation("org.flywaydb:flyway-core")
+    runtimeOnly("org.postgresql:postgresql")
+
+    // Mapper
+    implementation("org.mapstruct:mapstruct:1.5.3.Final")
+    kapt("org.mapstruct:mapstruct-processor:1.5.3.Final")
+
+    // Kafka messaging
+    implementation("org.springframework.kafka:spring-kafka")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+
+    //Kotlinx coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+
+    //Kotlinx serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+
+    // Utilities
+    implementation("one.stayfocused.spring:dotenv-spring-boot:1.0.0")
+
+    // Tests
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Metrics
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("io.zipkin.reporter2:zipkin-reporter-brave")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+conventionalCommits {
+    warningIfNoGitRoot = true
+    types += listOf("build", "chore", "docs", "feat", "fix", "refactor", "style", "test")
+    scopes = emptyList()
+    successMessage = "Сообщение коммита соответствует стандартам Conventional Commit."
+    failureMessage = "Сообщение коммита не соответствует стандартам Conventional Commit."
+}
