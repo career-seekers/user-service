60f48ce0d4c3d067471839c072479a586c40a794
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-07-09 02:07:41 +0300

    feat(database): created expert docs table

---
+++ src/main/kotlin/org/careerseekers/userservice/repositories/ExpertDocsRepository.kt
@@ -0,0 +1,8 @@
+package org.careerseekers.userservice.repositories
+
+import org.careerseekers.userservice.entities.ExpertDocuments
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ExpertDocsRepository : JpaRepository<ExpertDocuments, Long>
