c6b2041533488e13ccc8d48611f5156594937f10
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-07-16 00:23:40 +0300

    fix(utils): fixed mobile formatter

--- src/main/kotlin/org/careerseekers/userservice/utils/MobileNumberFormatter.kt
+++ src/main/kotlin/org/careerseekers/userservice/utils/MobileNumberFormatter.kt
@@ -4,7 +4,7 @@ import org.careerseekers.userservice.exceptions.MobileNumberFormatException
 
 object MobileNumberFormatter {
     fun checkMobileNumberValid(mobile: String) {
-        val charsValid = mobile.all { it.isDigit() || it == '+'}
+        val charsValid = mobile.startsWith('+') && mobile.substring(1).all { it.isDigit() }
 
         if (mobile.length != 12 || !charsValid) {
             throw MobileNumberFormatException("Mobile number must be 12 characters long in format '+79991234567'")
