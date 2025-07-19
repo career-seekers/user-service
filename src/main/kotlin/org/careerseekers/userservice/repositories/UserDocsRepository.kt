2a532acfb47080b15a9925c545c195fa4f526317
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-07-20 00:28:41 +0300

    fix(repository): fixed findByUserId returns class

--- src/main/kotlin/org/careerseekers/userservice/repositories/UserDocsRepository.kt
+++ src/main/kotlin/org/careerseekers/userservice/repositories/UserDocsRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserDocsRepository : JpaRepository<UserDocuments, Long>
+interface UserDocsRepository : JpaRepository<UserDocuments, Long> {
+    fun findByUserId(userId: Long): UserDocuments
+}
