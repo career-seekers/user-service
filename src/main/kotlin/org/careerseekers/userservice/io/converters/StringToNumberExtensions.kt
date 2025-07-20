e1453dde8610713bb435aead10120ced606de8e5
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-07-21 02:40:29 +0300

    feat(validators): create custom function for casting string to number

---
+++ src/main/kotlin/org/careerseekers/userservice/io/converters/StringToNumberExtensions.kt
@@ -0,0 +1,9 @@
+package org.careerseekers.userservice.io.converters
+
+import org.careerseekers.userservice.exceptions.InvalidNumberFormatException
+
+fun String.toLongOrThrow(message: String = "Invalid long value '$this'"): Long =
+    this.toLongOrNull() ?: throw InvalidNumberFormatException(message)
+
+fun String.toShortOrThrow(message: String = "Invalid short value '$this'"): Short =
+    this.toShortOrNull() ?: throw InvalidNumberFormatException(message)
