009c734c2be8a457a8fba0f01954d9233b4832c2
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-01 18:09:38 +0300

    feat(auth): setup security context

---
+++ src/main/kotlin/org/careerseekers/userservice/security/CustomAccessDeniedHandler.kt
@@ -0,0 +1,20 @@
+package org.careerseekers.userservice.security
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.web.access.AccessDeniedHandler
+import org.springframework.stereotype.Component
+
+@Component
+class CustomAccessDeniedHandler : AccessDeniedHandler {
+    override fun handle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        accessDeniedException: AccessDeniedException
+    ) {
+        response.status = HttpServletResponse.SC_FORBIDDEN
+        response.contentType = "application/json"
+        response.writer.write("""{"status":403,"message":"Access denied"}""")
+    }
+}
