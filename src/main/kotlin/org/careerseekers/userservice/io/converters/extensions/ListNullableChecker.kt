55ae368f4a912f9bd7b85f28cb0d66c091fb8b66
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-01 13:36:34 +0300

    feat(utils): create ListNullableChecker.kt

--- src/main/kotlin/org/careerseekers/userservice/io/converters/extensions/ListNullableChecker.kt
+++ src/main/kotlin/org/careerseekers/userservice/io/converters/extensions/ListNullableChecker.kt
@@ -10,9 +10,9 @@ import org.careerseekers.userservice.exceptions.BadRequestException
  *
  * @throws BadRequestException if the list contains a mixture of null and non-null elements.
  */
-fun <T> List<T?>.checkNullable() {
+fun <T> List<T?>.checkNullable(message: String = "These parameters can be only all null values or all non-null values.") {
     val nullCount = this.count { it == null }
     if (nullCount > 0 && nullCount < this.size) {
-        throw BadRequestException("These parameters can be only all null values or all non-null values.")
+        throw BadRequestException(message)
     }
 }
