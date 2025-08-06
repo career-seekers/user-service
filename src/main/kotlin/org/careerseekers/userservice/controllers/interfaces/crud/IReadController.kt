8cbcb6f431ca7cb3e56f590113b93c53bb04ebc0
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-06 18:09:03 +0300

    feat(tutor-docs): created tutor documents controller

--- src/main/kotlin/org/careerseekers/userservice/controllers/interfaces/crud/IReadController.kt
+++ src/main/kotlin/org/careerseekers/userservice/controllers/interfaces/crud/IReadController.kt
@@ -1,10 +1,11 @@
 package org.careerseekers.userservice.controllers.interfaces.crud
 
+import org.careerseekers.userservice.io.BasicSuccessfulResponse
 import org.careerseekers.userservice.services.interfaces.crud.IReadService
 
 interface IReadController<T, ID> {
     val service: IReadService<T, ID>
 
-    fun getAll(): List<T>
-    fun getById(id: Long): T?
+    fun getAll(): BasicSuccessfulResponse<List<T>>
+    fun getById(id: Long): BasicSuccessfulResponse<T>
 }
