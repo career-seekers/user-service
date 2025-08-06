718653df1d15b75ab08538afc83f2a51c931eb6c
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-06 18:21:00 +0300

    refactor(core): create logical deps between crud interfaces

--- src/main/kotlin/org/careerseekers/userservice/controllers/interfaces/crud/IReadController.kt
+++ src/main/kotlin/org/careerseekers/userservice/controllers/interfaces/crud/IReadController.kt
@@ -1,10 +1,11 @@
 package org.careerseekers.userservice.controllers.interfaces.crud
 
+import org.careerseekers.userservice.controllers.interfaces.BasicRestController
 import org.careerseekers.userservice.io.BasicSuccessfulResponse
 import org.careerseekers.userservice.services.interfaces.crud.IReadService
 
-interface IReadController<T, ID> {
-    val service: IReadService<T, ID>
+interface IReadController<T, ID> : BasicRestController {
+    override val service: IReadService<T, ID>
 
     fun getAll(): BasicSuccessfulResponse<List<T>>
     fun getById(id: ID): BasicSuccessfulResponse<T>
