7828563f80e2e0a0b81f9a5021825ce1913df1f8
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-07 23:51:25 +0300

    refactor(core): renamed class for exceptions handling

--- src/main/kotlin/org/careerseekers/userservice/io/handlers/GlobalHttpExceptionHandler.kt
+++ src/main/kotlin/org/careerseekers/userservice/io/handlers/GlobalHttpExceptionHandler.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
-class GlobalExceptionHandler {
+class GlobalHttpExceptionHandler {
 
     /**
      * Basic exceptions handler
