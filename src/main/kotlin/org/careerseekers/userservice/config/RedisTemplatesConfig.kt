f7a31855954fa2ac403c2cff83f6a30b900599a3
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-10 01:26:57 +0300

    refactor(core): extract object mapper creating to public function

--- src/main/kotlin/org/careerseekers/userservice/config/RedisTemplatesConfig.kt
+++ src/main/kotlin/org/careerseekers/userservice/config/RedisTemplatesConfig.kt
@@ -1,9 +1,5 @@
 package org.careerseekers.userservice.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.careerseekers.userservice.entities.JwtTokensStorage
 import org.careerseekers.userservice.entities.Users
 import org.springframework.beans.factory.annotation.Qualifier
@@ -24,11 +20,7 @@ class RedisTemplatesConfig {
         val template = RedisTemplate<String, JwtTokensStorage>()
         template.connectionFactory = connectionFactory
 
-        val objectMapper = ObjectMapper()
-            .registerKotlinModule()
-            .registerModule(JavaTimeModule())
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-
+        val objectMapper = createConfiguredObjectMapper()
         val serializer = GenericJackson2JsonRedisSerializer(objectMapper)
 
         template.keySerializer = StringRedisSerializer()
@@ -49,11 +41,7 @@ class RedisTemplatesConfig {
         val template = RedisTemplate<String, Users>()
         template.connectionFactory = connectionFactory
 
-        val objectMapper = ObjectMapper()
-            .registerKotlinModule()
-            .registerModule(JavaTimeModule())
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-
+        val objectMapper = createConfiguredObjectMapper()
         val serializer = GenericJackson2JsonRedisSerializer(objectMapper)
 
         template.keySerializer = StringRedisSerializer()
