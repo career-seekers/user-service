5bf9c0bd7cbe9baf8348fb4c144b596f7793c7f8
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-10 23:39:49 +0300

    refactor(caches): change separate cache interfaces

--- src/main/kotlin/org/careerseekers/userservice/cache/CacheLoader.kt
+++ src/main/kotlin/org/careerseekers/userservice/cache/CacheLoader.kt
@@ -1,19 +1,6 @@
 package org.careerseekers.userservice.cache
 
-import org.springframework.data.redis.core.RedisTemplate
-
-interface CacheLoader<T> {
-    val cacheKey: String
-    val redisTemplate: RedisTemplate<String, T>
-
-    fun getAllFromCache(): List<T> {
-        val keys = redisTemplate.keys("$cacheKey::*")
-        val ops = redisTemplate.opsForValue()
-
-        return keys.mapNotNull { ops.get(it) }
-    }
-
+interface CacheLoader<T> : CacheClient<T> {
     fun preloadCache(): Any
     fun loadItemToCache(item: T): Any
-    fun getItemFromCache(key: Any): T?
 }
