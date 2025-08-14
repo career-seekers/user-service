cf0616570ada8f719b9ea489367f0994b2804336
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-15 02:24:33 +0300

    feat(caches): created cache client for verification codes caches

---
+++ src/main/kotlin/org/careerseekers/userservice/cache/VerificationCodesCacheClient.kt
@@ -0,0 +1,28 @@
+package org.careerseekers.userservice.cache
+
+import org.careerseekers.userservice.dto.VerificationCodeDto
+import org.springframework.cache.CacheManager
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class VerificationCodesCacheClient(
+    override val redisTemplate: RedisTemplate<String, VerificationCodeDto>,
+    cacheManager: CacheManager,
+) : CacheClient<VerificationCodeDto> {
+    override val cacheKey = "verification_codes"
+    private val cache = cacheManager.getCache(cacheKey)
+
+
+    override fun getItemFromCache(key: Any): VerificationCodeDto? {
+        return cache?.get(key)?.let { it.get() as? VerificationCodeDto }
+    }
+
+    fun deleteItemFromCache(key: Any) {
+        cache?.evictIfPresent(key)
+    }
+
+    fun loadItemToCache(userId: Long, item: VerificationCodeDto) {
+        cache?.putIfAbsent(userId, item)
+    }
+}
