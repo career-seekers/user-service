e70d73844ed1494465a94ab86a86e572ddf3ca6a
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-14 23:59:22 +0300

    feat(kafka): created producer for email sending topic

---
+++ src/main/kotlin/org/careerseekers/userservice/services/kafka/producers/CustomKafkaProducer.kt
@@ -0,0 +1,12 @@
+package org.careerseekers.userservice.services.kafka.producers
+
+import org.careerseekers.userservice.dto.KafkaMessagesDto
+import org.careerseekers.userservice.enums.KafkaTopics
+import org.springframework.kafka.core.KafkaTemplate
+
+interface CustomKafkaProducer<T : KafkaMessagesDto> {
+    val topic: KafkaTopics
+    val template: KafkaTemplate<String, T>
+
+    fun sendMessage(message: T)
+}
