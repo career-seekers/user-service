b5639ad43dd9b04d4a926bf4a0aca4b452c2902c
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-15 02:33:53 +0300

    refactor(dto): update ChangePasswordSecondStepDto.kt

--- src/main/kotlin/org/careerseekers/userservice/dto/users/ChangePasswordSecondStepDto.kt
+++ src/main/kotlin/org/careerseekers/userservice/dto/users/ChangePasswordSecondStepDto.kt
@@ -3,7 +3,6 @@ package org.careerseekers.userservice.dto.users
 import org.careerseekers.userservice.dto.DtoClass
 
 data class ChangePasswordSecondStepDto(
-    val jwtToken: String,
     val verificationCode: String,
     val newPassword: String,
 ) : DtoClass
