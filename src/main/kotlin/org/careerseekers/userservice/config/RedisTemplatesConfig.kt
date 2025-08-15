53368d7e8c1403a8ce7934a6d205ad72a477d52b
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-15 13:21:43 +0300

    feat(cache): created redis template for verification codes caches
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/main/kotlin/org/careerseekers/userservice/config/RedisTemplatesConfig.kt
+++ src/main/kotlin/org/careerseekers/userservice/config/RedisTemplatesConfig.kt
@@ -2,6 +2,7 @@ package org.careerseekers.userservice.config
 
 import org.careerseekers.userservice.dto.CachesDto
 import org.careerseekers.userservice.dto.UsersCacheDto
+import org.careerseekers.userservice.dto.VerificationCodeDto
 import org.careerseekers.userservice.serializers.PolymorphicRedisSerializer
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
@@ -21,6 +22,24 @@ class RedisTemplatesConfig(
         connectionFactory: RedisConnectionFactory
     ): RedisTemplate<String, UsersCacheDto> {
         val template = RedisTemplate<String, UsersCacheDto>()
+        template.connectionFactory = connectionFactory
+
+        template.keySerializer = StringRedisSerializer()
+        template.valueSerializer = serializer
+
+        template.hashKeySerializer = StringRedisSerializer()
+        template.hashValueSerializer = serializer
+
+        template.afterPropertiesSet()
+        return template
+    }
+
+    @Bean
+    @Qualifier("verificationCodes")
+    fun verificationCodesRedisTemplate(
+        connectionFactory: RedisConnectionFactory
+    ): RedisTemplate<String, VerificationCodeDto> {
+        val template = RedisTemplate<String, VerificationCodeDto>()
         template.connectionFactory = connectionFactory
 
         template.keySerializer = StringRedisSerializer()
