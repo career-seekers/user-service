5f30a9a419bc2576cd87264520d6ea32b13c5b25
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-16 13:06:26 +0300

    feat(utils): created PasswordGenerator.kt for experts registration
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

---
+++ src/main/kotlin/org/careerseekers/userservice/utils/PasswordGenerator.kt
@@ -0,0 +1,20 @@
+package org.careerseekers.userservice.utils
+
+import kotlin.random.Random
+
+object PasswordGenerator {
+    private const val PASSWORD_LENGTH = 16
+    private val CHAR_POOL: List<Char> = (
+            ('a'..'z').toList() +
+                    ('A'..'Z').toList() +
+                    ('0'..'9').toList() +
+                    "!@#\$%^&*()-_=+[]{}<>?".toList()
+            )
+
+    fun generatePassword(): String {
+        return (1..PASSWORD_LENGTH)
+            .map { Random.nextInt(0, CHAR_POOL.size) }
+            .map(CHAR_POOL::get)
+            .joinToString("")
+    }
+}
