4c5366c80d381c9d71301eac3ce88e04f5b3f144
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-20 01:00:10 +0300

    refactor(db): remove unnecessary table
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/main/kotlin/org/careerseekers/userservice/repositories/ChildToMentorRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.careerseekers.userservice.repositories
-
-import org.careerseekers.userservice.entities.ChildToMentor
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ChildToMentorRepository : JpaRepository<ChildToMentor, Long>
