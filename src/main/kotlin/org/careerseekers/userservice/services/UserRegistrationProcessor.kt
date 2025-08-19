a7ad86e8ea24486a18ee206a33abff5cf9ed18d9
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-19 13:47:04 +0300

    feat(services): created UserRegistrationProcessor.kt
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

---
+++ src/main/kotlin/org/careerseekers/userservice/services/UserRegistrationProcessor.kt
@@ -0,0 +1,79 @@
+package org.careerseekers.userservice.services
+
+import org.careerseekers.userservice.dto.EmailSendingTaskDto
+import org.careerseekers.userservice.dto.users.CreateChildDto
+import org.careerseekers.userservice.dto.auth.RegisterUserDto
+import org.careerseekers.userservice.dto.auth.RegisterUserExternalDto
+import org.careerseekers.userservice.dto.auth.RegistrationDto
+import org.careerseekers.userservice.dto.users.CreateMentorToChildDto
+import org.careerseekers.userservice.entities.Users
+import org.careerseekers.userservice.enums.MailEventTypes
+import org.careerseekers.userservice.enums.UsersRoles
+import org.careerseekers.userservice.exceptions.BadRequestException
+import org.careerseekers.userservice.io.converters.extensions.toCache
+import org.careerseekers.userservice.mappers.ChildrenMapper
+import org.careerseekers.userservice.mappers.ChildrenToMentorMapper
+import org.careerseekers.userservice.repositories.ChildToMentorRepository
+import org.careerseekers.userservice.repositories.ChildrenRepository
+import org.careerseekers.userservice.repositories.UsersRepository
+import org.careerseekers.userservice.services.interfaces.IUserNotificationProcessor
+import org.careerseekers.userservice.services.interfaces.IUsersRegistrationProcessor
+import org.careerseekers.userservice.services.kafka.producers.KafkaEmailSendingProducer
+import org.springframework.stereotype.Service
+
+@Service
+class UserRegistrationProcessor(
+    private val childrenRepository: ChildrenRepository,
+    private val childrenMapper: ChildrenMapper,
+    private val childToMentorRepository: ChildToMentorRepository,
+    private val childrenToMentorMapper: ChildrenToMentorMapper,
+    private val usersService: UsersService,
+    private val usersRepository: UsersRepository,
+    private val emailSendingProducer: KafkaEmailSendingProducer,
+) : IUsersRegistrationProcessor<RegistrationDto>, IUserNotificationProcessor {
+    override val userRole = UsersRoles.USER
+
+    override fun processRegistration(item: RegistrationDto) {
+        when (item) {
+            is RegisterUserDto -> processUserRegistration()
+            is RegisterUserExternalDto -> processUserWithChildRegistration(item)
+        }
+    }
+
+    private fun processUserRegistration(): Nothing =
+        throw BadRequestException("Invalid data package for user registration")
+
+
+    private fun processUserWithChildRegistration(item: RegisterUserExternalDto) {
+        val user = usersService.getByEmail(item.email)!!
+        val mentor = item.mentorId?.let { usersService.getById(it) }
+
+        val child = CreateChildDto(
+            lastName = item.lastName,
+            firstName = item.firstName,
+            patronymic = item.patronymic,
+            user = user,
+            mentor = mentor
+        ).let(childrenMapper::childFromDto)
+            .run(childrenRepository::save)
+
+        CreateMentorToChildDto(child = child, mentor = mentor)
+            .let(childrenToMentorMapper::entityFromDto)
+            .run(childToMentorRepository::save)
+
+        if (item.mentorEqualsUser) {
+            user.isMentor = true
+            usersRepository.save(user)
+
+            notifyUser(user)
+        }
+    }
+
+    override fun notifyUser(user: Users) {
+        emailSendingProducer.sendMessage(EmailSendingTaskDto(
+            token = null,
+            eventType = MailEventTypes.MENTOR_AND_USER_REGISTRATION,
+            user = user.toCache(),
+        ))
+    }
+}
