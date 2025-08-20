c6c81c766e39fba46370a3250ace18d7be677dee
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-20 04:02:47 +0300

    refactor(service): send to kafka user, not email
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/main/kotlin/org/careerseekers/userservice/services/processors/ExpertRegistrationProcessor.kt
+++ src/main/kotlin/org/careerseekers/userservice/services/processors/ExpertRegistrationProcessor.kt
@@ -4,19 +4,24 @@ import org.careerseekers.userservice.dto.EmailSendingTaskDto
 import org.careerseekers.userservice.dto.auth.RegistrationDto
 import org.careerseekers.userservice.enums.MailEventTypes
 import org.careerseekers.userservice.enums.UsersRoles
+import org.careerseekers.userservice.io.converters.extensions.toCache
+import org.careerseekers.userservice.services.UsersService
 import org.careerseekers.userservice.services.kafka.producers.KafkaEmailSendingProducer
 import org.springframework.stereotype.Service
 
 @Service
 class ExpertRegistrationProcessor(
     private val emailSendingProducer: KafkaEmailSendingProducer,
+    private val usersService: UsersService,
 ) : IUsersRegistrationProcessor {
     override val userRole = UsersRoles.EXPERT
 
     override fun <T : RegistrationDto> processRegistration(item: T) {
-        emailSendingProducer.sendMessage(EmailSendingTaskDto(
-            email = item.email,
-            eventType = MailEventTypes.EXPERT_REGISTRATION,
-        ))
+        usersService.getByEmail(item.email)?.let { user ->
+            emailSendingProducer.sendMessage(EmailSendingTaskDto(
+                user = user.toCache(),
+                eventType = MailEventTypes.EXPERT_REGISTRATION,
+            ))
+        }
     }
 }
