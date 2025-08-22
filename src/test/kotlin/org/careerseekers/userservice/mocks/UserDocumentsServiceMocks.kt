59c9dc82df04a5984b10b45d511b3d1acc34455c
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-22 20:14:00 +0300

    test(service): created UserDocumentsService mocks
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

---
+++ src/test/kotlin/org/careerseekers/userservice/mocks/UserDocumentsServiceMocks.kt
@@ -0,0 +1,29 @@
+package org.careerseekers.userservice.mocks
+
+import io.mockk.mockk
+import org.careerseekers.userservice.mappers.UserDocumentsMapper
+import org.careerseekers.userservice.repositories.UserDocsRepository
+import org.careerseekers.userservice.repositories.UsersRepository
+import org.careerseekers.userservice.services.UserDocumentsService
+import org.careerseekers.userservice.services.UsersService
+import org.careerseekers.userservice.utils.DocumentsApiResolver
+import org.careerseekers.userservice.utils.SnilsValidator
+
+open class UserDocumentsServiceMocks {
+    protected val repository = mockk<UserDocsRepository>()
+    protected val usersRepository = mockk<UsersRepository>()
+    protected val usersService = mockk<UsersService>()
+    protected val documentsApiResolver = mockk<DocumentsApiResolver>()
+    protected val userDocumentsMapper = mockk<UserDocumentsMapper>()
+    protected val snilsValidator = mockk<SnilsValidator>()
+
+    protected val usersServiceMock = mockk<UserDocumentsService>(relaxed = true)
+    protected val serviceUnderTest = UserDocumentsService(
+        repository = repository,
+        usersRepository = usersRepository,
+        usersService = usersService,
+        documentsApiResolver = documentsApiResolver,
+        userDocumentsMapper = userDocumentsMapper,
+        snilsValidator = snilsValidator
+    )
+}
