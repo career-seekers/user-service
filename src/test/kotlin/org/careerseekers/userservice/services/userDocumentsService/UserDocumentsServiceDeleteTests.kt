e027fe0bfae683d15abc7009f6b7f573f45be8ec
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-23 00:40:39 +0300

    test(service): created tests deleteById function
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

---
+++ src/test/kotlin/org/careerseekers/userservice/services/userDocumentsService/UserDocumentsServiceDeleteTests.kt
@@ -0,0 +1,60 @@
+package org.careerseekers.userservice.services.userDocumentsService
+
+import io.mockk.every
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.careerseekers.userservice.exceptions.NotFoundException
+import org.careerseekers.userservice.mocks.UserDocumentsServiceMocks
+import org.careerseekers.userservice.mocks.generators.DocumentsGenerator.createUserDocs
+import org.careerseekers.userservice.mocks.generators.UsersGenerator.createUser
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+class UserDocumentsServiceDeleteTests : UserDocumentsServiceMocks() {
+
+    @Nested
+    inner class DeleteByIdTests {
+
+        @Test
+        fun `Should set userDocs to null and delete it`() {
+            val user = createUser()
+            val documents = createUserDocs(user)
+
+            every { serviceUnderTest.getById(documents.id, true) } returns documents
+            every { usersRepository.save(any()) } returns user
+            every { repository.delete(any()) } returns Unit
+            every { serviceUnderTest["removeDocumentsFromDatabase"](documents) } returns Unit
+
+            val result = serviceUnderTest.deleteById(documents.id)
+
+            assertThat(result).isEqualTo("User documents deleted successfully.")
+            assertThat(user.userDocuments).isNull()
+
+            verify { serviceUnderTest.getById(documents.id, true) }
+            verify { usersRepository.save(any()) }
+            verify { repository.delete(any()) }
+            verify { serviceUnderTest["removeDocumentsFromDatabase"](documents) }
+        }
+
+        @Test
+        fun `Should return NotFoundException when user documents not found`() {
+            val user = createUser()
+            val documents = createUserDocs(user)
+
+            every { serviceUnderTest.getById(documents.id, true) } throws NotFoundException("User documents with id ${documents.id} not found")
+
+            val exception = assertFailsWith<NotFoundException> {
+                serviceUnderTest.deleteById(documents.id)
+            }
+
+            assertThat(exception.message).isEqualTo("User documents with id ${documents.id} not found")
+
+            verify { serviceUnderTest.getById(documents.id, true) }
+
+            verify(exactly = 0) { usersRepository.save(any()) }
+            verify(exactly = 0) { repository.delete(any()) }
+            verify(exactly = 0) { serviceUnderTest["removeDocumentsFromDatabase"](documents) }
+        }
+    }
+}
