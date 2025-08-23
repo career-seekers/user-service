13bf75d5a912c1b48a4717abff976800423148bb
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-23 14:37:32 +0300

    test(service): created tests for getByUserId function in tutor documents service
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/test/kotlin/org/careerseekers/userservice/services/tutorDocumentsService/TutorDocumentsServiceGetTests.kt
+++ src/test/kotlin/org/careerseekers/userservice/services/tutorDocumentsService/TutorDocumentsServiceGetTests.kt
@@ -5,6 +5,7 @@ import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.careerseekers.userservice.entities.TutorDocuments
 import org.careerseekers.userservice.enums.UsersRoles
+import org.careerseekers.userservice.exceptions.BadRequestException
 import org.careerseekers.userservice.exceptions.NotFoundException
 import org.careerseekers.userservice.mocks.TutorDocumentsServiceMocks
 import org.careerseekers.userservice.mocks.generators.DocumentsGenerator.createTutorDocuments
@@ -81,6 +82,96 @@ class TutorDocumentsServiceGetTests : TutorDocumentsServiceMocks() {
 
             assertThat(result).isNull()
             verify { repository.findById(any()) }
+        }
+    }
+
+    @Nested
+    inner class GetDocsByUserIdTests {
+
+        @Test
+        fun `Should return tutor documents by user id`() {
+            val user = createUser().copy(role = UsersRoles.TUTOR)
+            val documents = createTutorDocuments(user)
+
+            every { usersService.getById(user.id, any(), any()) } returns user
+            every { repository.findByUserId(user.id) } returns documents
+
+            val result = serviceUnderTest.getDocsByUserId(user.id)
+
+            assertThat(result).isEqualTo(documents)
+
+            verify { usersService.getById(user.id, any(), any()) }
+            verify { repository.findByUserId(user.id) }
+        }
+
+        @Test
+        fun `Should throw NotFoundException when documents not found and throwable = true`() {
+            val user = createUser().copy(role = UsersRoles.TUTOR)
+
+            every { usersService.getById(user.id, any(), any()) } returns user
+            every { repository.findByUserId(user.id) } returns null
+
+            val exception = assertFailsWith<NotFoundException> {
+                serviceUnderTest.getDocsByUserId(user.id)
+            }
+
+            assertThat(exception.message).isEqualTo("Documents for user with id ${user.id} not found")
+
+            verify { usersService.getById(user.id, any(), any()) }
+            verify { repository.findByUserId(user.id) }
+        }
+
+        @Test
+        fun `Should return null when documents not found and throwable = false`() {
+            val user = createUser().copy(role = UsersRoles.TUTOR)
+
+            every { usersService.getById(user.id, any(), any()) } returns user
+            every { repository.findByUserId(user.id) } returns null
+
+            val result = serviceUnderTest.getDocsByUserId(user.id, throwable = false)
+
+            assertThat(result).isNull()
+
+            verify { usersService.getById(user.id, any(), any()) }
+            verify { repository.findByUserId(user.id) }
+        }
+
+        @Test
+        fun `Should return NotFoundException when user not found`() {
+            val user = createUser().copy(role = UsersRoles.TUTOR)
+
+            every {
+                usersService.getById(
+                    user.id,
+                    any(),
+                    any()
+                )
+            } throws NotFoundException("User with id ${user.id} not found")
+
+            val exception = assertFailsWith<NotFoundException> {
+                serviceUnderTest.getDocsByUserId(user.id)
+            }
+
+            assertThat(exception.message).isEqualTo("User with id ${user.id} not found")
+
+            verify { usersService.getById(user.id, any(), any()) }
+            verify(exactly = 0) { repository.findByUserId(user.id) }
+        }
+
+        @Test
+        fun `Should throw BadRequestException if user role is not TUTOR`() {
+            val user = createUser().copy(role = UsersRoles.ADMIN)
+
+            every { usersService.getById(user.id, any(), any()) } returns user
+
+            val exception = assertFailsWith<BadRequestException> {
+                serviceUnderTest.getDocsByUserId(user.id)
+            }
+
+            assertThat(exception.message).isEqualTo("This user has role ${user.role}, not ${UsersRoles.TUTOR}. Please use another controller to check his documents.")
+
+            verify { usersService.getById(user.id, any(), any()) }
+            verify(exactly = 0) { repository.findByUserId(user.id) }
         }
     }
 }
