b8af9915b0c5a4f11d1e30d9f71b64d2bc851e9e
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-23 22:56:31 +0300

    fix(service): fix throwable exception message
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/main/kotlin/org/careerseekers/userservice/services/MentorDocumentsService.kt
+++ src/main/kotlin/org/careerseekers/userservice/services/MentorDocumentsService.kt
@@ -38,7 +38,7 @@ class MentorDocumentsService(
         return usersService.getById(userId, message = "User with id $userId not found").let { user ->
             if (user!!.role == UsersRoles.MENTOR) {
                 repository.findByUserId(user.id)
-                    ?: if (throwable) throw NotFoundException("Documents for user with if $userId not found") else null
+                    ?: if (throwable) throw NotFoundException("Documents for user with id $userId not found") else null
             } else {
                 throw BadRequestException(
                     "This user has role ${user.role}, not ${UsersRoles.MENTOR}. Please use another controller to check his documents."
