800b9989c4fbf9cd9acda6c2f7aec5c782cbb06d
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-23 14:33:35 +0300

    test(service): created tests for getById tutor docs function
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/test/kotlin/org/careerseekers/userservice/services/tutorDocumentsService/TutorDocumentsServiceGetTests.kt
+++ src/test/kotlin/org/careerseekers/userservice/services/tutorDocumentsService/TutorDocumentsServiceGetTests.kt
@@ -4,11 +4,15 @@ import io.mockk.every
 import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.careerseekers.userservice.entities.TutorDocuments
+import org.careerseekers.userservice.enums.UsersRoles
+import org.careerseekers.userservice.exceptions.NotFoundException
 import org.careerseekers.userservice.mocks.TutorDocumentsServiceMocks
 import org.careerseekers.userservice.mocks.generators.DocumentsGenerator.createTutorDocuments
 import org.careerseekers.userservice.mocks.generators.UsersGenerator.createUser
 import org.junit.jupiter.api.Nested
+import java.util.Optional
 import kotlin.test.Test
+import kotlin.test.assertFailsWith
 
 class TutorDocumentsServiceGetTests : TutorDocumentsServiceMocks() {
 
@@ -17,7 +21,7 @@ class TutorDocumentsServiceGetTests : TutorDocumentsServiceMocks() {
 
         @Test
         fun `Should return a list of tutor documents`() {
-            val documents = List(5) { createTutorDocuments(createUser()) }
+            val documents = List(5) { createTutorDocuments(createUser().copy(role = UsersRoles.TUTOR)) }
 
             every { repository.findAll() } returns documents
 
@@ -37,6 +41,46 @@ class TutorDocumentsServiceGetTests : TutorDocumentsServiceMocks() {
 
             assertThat(result).isEqualTo(documents)
             verify { repository.findAll() }
+        }
+    }
+
+    @Nested
+    inner class GetByIdTests {
+
+        @Test
+        fun `Should return tutor document by id`() {
+            val documents = createTutorDocuments(createUser().copy(role = UsersRoles.TUTOR))
+
+            every { repository.findById(documents.id) } returns Optional.of(documents)
+
+            val result = serviceUnderTest.getById(documents.id)
+
+            assertThat(result).isNotNull.isEqualTo(documents)
+            verify { repository.findById(documents.id) }
+        }
+
+        @Test
+        fun `Should return NotFoundException when documents not found and throwable = true`() {
+            val documents = createTutorDocuments(createUser().copy(role = UsersRoles.TUTOR))
+            every { repository.findById(any()) } returns Optional.empty()
+
+            val exception = assertFailsWith<NotFoundException> {
+                serviceUnderTest.getById(documents.id, message = "Tutor documents with id ${documents.id} not found")
+            }
+
+            assertThat(exception.message).isEqualTo("Tutor documents with id ${documents.id} not found")
+            verify { repository.findById(any()) }
+        }
+
+        @Test
+        fun `Should return null when documents not found and throwable = false`() {
+            val documents = createTutorDocuments(createUser().copy(role = UsersRoles.TUTOR))
+            every { repository.findById(any()) } returns Optional.empty()
+
+            val result = serviceUnderTest.getById(documents.id, false)
+
+            assertThat(result).isNull()
+            verify { repository.findById(any()) }
         }
     }
 }
