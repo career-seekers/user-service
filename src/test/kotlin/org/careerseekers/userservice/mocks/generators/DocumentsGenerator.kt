9f943e728a35d6bfac302349d0626174d05faba9
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-23 22:47:28 +0300

    feat(mocks): created functions for mentor docs generation
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/test/kotlin/org/careerseekers/userservice/mocks/generators/DocumentsGenerator.kt
+++ src/test/kotlin/org/careerseekers/userservice/mocks/generators/DocumentsGenerator.kt
@@ -1,9 +1,11 @@
 package org.careerseekers.userservice.mocks.generators
 
 import MocksGenerator.randomString
+import org.careerseekers.userservice.dto.docs.CreateMentorDocsDto
 import org.careerseekers.userservice.dto.docs.CreateTutorDocsDto
 import org.careerseekers.userservice.dto.docs.CreateUserDocsDto
 import org.careerseekers.userservice.dto.docs.SnilsDto
+import org.careerseekers.userservice.entities.MentorDocuments
 import org.careerseekers.userservice.entities.TutorDocuments
 import org.careerseekers.userservice.entities.UserDocuments
 import org.careerseekers.userservice.entities.Users
@@ -54,5 +56,20 @@ object DocumentsGenerator {
         institution = randomString(12),
         post = randomString(12),
         consentToTutorPdp = createMultipartFile()
+    )
+
+    fun createMentorDocuments(user: Users) = MentorDocuments(
+        id = Random.nextLong(1, 10000),
+        user = user,
+        institution = randomString(12),
+        post = randomString(12),
+        consentToMentorPdpId = Random.nextLong(1, 1000)
+    )
+
+    fun createMentorDocumentsDto(user: Users) = CreateMentorDocsDto(
+        userId = user.id,
+        institution = randomString(12),
+        post = randomString(12),
+        consentToMentorPdp = createMultipartFile()
     )
 }
