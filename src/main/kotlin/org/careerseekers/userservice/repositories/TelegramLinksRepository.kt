1d94ffe7f8dc1ca07871a6c033512ee021903780
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-08 01:07:23 +0300

    feat(core): telegram links mapper & repository
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

---
+++ src/main/kotlin/org/careerseekers/userservice/repositories/TelegramLinksRepository.kt
@@ -0,0 +1,8 @@
+package org.careerseekers.userservice.repositories
+
+import org.careerseekers.userservice.entities.TelegramLinks
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TelegramLinksRepository : JpaRepository<TelegramLinks, Long>
