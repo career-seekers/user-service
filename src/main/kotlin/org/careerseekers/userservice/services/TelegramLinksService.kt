f25627b9fe723e6e22bfd6a19ddd84b6a25c3799
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-08 01:07:41 +0300

    feat(service): telegram links service
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

---
+++ src/main/kotlin/org/careerseekers/userservice/services/TelegramLinksService.kt
@@ -0,0 +1,54 @@
+package org.careerseekers.userservice.services
+
+import org.careerseekers.userservice.dto.users.links.CreateTelegramLinksDto
+import org.careerseekers.userservice.dto.users.links.UpdateTelegramLinkDto
+import org.careerseekers.userservice.entities.TelegramLinks
+import org.careerseekers.userservice.mappers.TelegramLinksMapper
+import org.careerseekers.userservice.repositories.TelegramLinksRepository
+import org.careerseekers.userservice.services.interfaces.CrudService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class TelegramLinksService(
+    override val repository: TelegramLinksRepository,
+    private val telegramLinksMapper: TelegramLinksMapper,
+    private val usersService: UsersService,
+) : CrudService<TelegramLinks, Long, CreateTelegramLinksDto, UpdateTelegramLinkDto> {
+
+    @Transactional
+    override fun create(item: CreateTelegramLinksDto): TelegramLinks {
+        return usersService.getById(item.userId, message = "User with id ${item.userId} not found.").let { user ->
+            repository.save(telegramLinksMapper.linkFromDto(item.copy(user = user)))
+        }
+    }
+
+    @Transactional
+    override fun createAll(items: List<CreateTelegramLinksDto>): Any {
+        for (item in items) {
+            create(item)
+        }
+        return "Telegram links created successfully."
+    }
+
+    @Transactional
+    override fun update(item: UpdateTelegramLinkDto): String {
+        return getById(item.id, message = "Telegram link with id ${item.id} not found").let { link ->
+            link!!.tgLink = item.tgLink
+            "Telegram link updated successfully."
+        }
+    }
+
+    @Transactional
+    override fun deleteById(id: Long): Any? {
+        return getById(id, message = "Telegram link with id $id not found").let { link ->
+            repository.delete(link!!)
+            "Telegram link deleted successfully."
+        }
+    }
+
+    override fun deleteAll(): String {
+        super.deleteAll()
+        return "All telegram links deleted successfully."
+    }
+}
