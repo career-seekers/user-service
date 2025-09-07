4f094e055d5dd451b5a5886463f6dc34f19ad9b5
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-08 00:41:49 +0300

    feat(db): created entity for tg links storaging
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

---
+++ src/main/kotlin/org/careerseekers/userservice/entities/TelegramLinks.kt
@@ -0,0 +1,27 @@
+package org.careerseekers.userservice.entities
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.validation.constraints.NotBlank
+
+@Entity
+data class TelegramLinks(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(nullable = false, unique = true)
+    @field:NotBlank
+    var tgLink: String?,
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL], optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    var user: Users,
+)
--- src/main/kotlin/org/careerseekers/userservice/entities/Users.kt
+++ src/main/kotlin/org/careerseekers/userservice/entities/Users.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -16,7 +17,7 @@ import java.util.Date
 
 @Entity
 @Table(name = "users")
-data class Users (
+data class Users(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long,
@@ -80,5 +81,9 @@ data class Users (
 
     @JsonIgnore
     @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
-    var children: MutableList<Children>? = mutableListOf()
+    var children: MutableList<Children>? = mutableListOf(),
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "user", cascade = [CascadeType.ALL], fetch = FetchType.LAZY, orphanRemoval = true)
+    var telegramLink: TelegramLinks? = null
 ) : ConvertableToHttpResponse<Users>
