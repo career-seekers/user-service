34f9a888ac4307e00a14a2c3d51190cc073f8f7f
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-15 02:53:16 +0300

    fix(api): changed update method signature
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/main/kotlin/org/careerseekers/userservice/controllers/ExpertDocumentsController.kt
+++ src/main/kotlin/org/careerseekers/userservice/controllers/ExpertDocumentsController.kt
@@ -7,7 +7,6 @@ import org.careerseekers.userservice.dto.docs.UpdateExpertDocsDto
 import org.careerseekers.userservice.entities.ExpertDocuments
 import org.careerseekers.userservice.io.BasicSuccessfulResponse
 import org.careerseekers.userservice.io.converters.extensions.toHttpResponse
-import org.careerseekers.userservice.io.converters.extensions.toLongOrThrow
 import org.careerseekers.userservice.services.ExpertDocumentsService
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -17,9 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/users-service/v1/expert-docs")
@@ -42,21 +39,8 @@ class ExpertDocumentsController(
 
 
     @PatchMapping("/")
-    fun update(
-        @RequestPart("id") id: String,
-        @RequestPart("institution", required = false) institution: String?,
-        @RequestPart("post", required = false) post: String?,
-        @RequestPart("consentToExpertPdp", required = false) consentToExpertPdp: MultipartFile,
-    ): BasicSuccessfulResponse<String> {
-        val body = UpdateExpertDocsDto(
-            id = id.toLongOrThrow(),
-            institution = institution,
-            post = post,
-            consentToExpertPdp = consentToExpertPdp
-        )
-
-        return service.update(body).toHttpResponse()
-    }
+    fun update(@RequestBody item: UpdateExpertDocsDto): BasicSuccessfulResponse<String> =
+        service.update(item).toHttpResponse()
 
     @DeleteMapping("/{id}")
     override fun deleteById(@PathVariable id: Long): BasicSuccessfulResponse<String> {
