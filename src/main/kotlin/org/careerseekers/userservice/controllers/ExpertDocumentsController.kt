2f1659760524d3585014d78fb72f7ffdb6df0008
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-15 02:45:19 +0300

    refactor(api): updated expert docs controller
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/main/kotlin/org/careerseekers/userservice/controllers/ExpertDocumentsController.kt
+++ src/main/kotlin/org/careerseekers/userservice/controllers/ExpertDocumentsController.kt
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
@@ -36,21 +37,9 @@ class ExpertDocumentsController(
     fun getDocsByUserId(@PathVariable id: Long) = service.getDocsByUserId(id)
 
     @PostMapping("/")
-    fun create(
-        @RequestPart("userId") userId: String,
-        @RequestPart("institution") institution: String,
-        @RequestPart("post") post: String,
-        @RequestPart("consentToExpertPdp") consentToExpertPdp: MultipartFile,
-    ): BasicSuccessfulResponse<ExpertDocuments> {
-        val body = CreateExpertDocsDto(
-            userId = userId.toLongOrThrow(),
-            institution = institution,
-            post = post,
-            consentToExpertPdp = consentToExpertPdp,
-        )
+    fun create(@RequestBody item: CreateExpertDocsDto): BasicSuccessfulResponse<ExpertDocuments> =
+        service.create(item).toHttpResponse()
 
-        return service.create(body).toHttpResponse()
-    }
 
     @PatchMapping("/")
     fun update(
