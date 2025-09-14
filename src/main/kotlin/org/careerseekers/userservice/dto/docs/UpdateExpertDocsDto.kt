eaf91f7e958eb40b31ed720392ac4a2d986581a3
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-15 02:54:40 +0300

    fix(dto): fixed update expert docs dto
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/main/kotlin/org/careerseekers/userservice/dto/docs/UpdateExpertDocsDto.kt
+++ src/main/kotlin/org/careerseekers/userservice/dto/docs/UpdateExpertDocsDto.kt
@@ -1,7 +1,6 @@
 package org.careerseekers.userservice.dto.docs
 
 import org.careerseekers.userservice.dto.DtoClass
-import org.springframework.web.multipart.MultipartFile
 
 data class UpdateExpertDocsDto(
     val id: Long,
