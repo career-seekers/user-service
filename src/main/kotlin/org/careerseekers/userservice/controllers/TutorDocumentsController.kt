2675e3bebc6f4e081442006891d925b9f9cb2c4f
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-17 00:21:07 +0300

    refactor(api): updated tutor docs controller
    Signed-off-by: Vladimir Fokin <115186975+scobca@users.noreply.github.com>

--- src/main/kotlin/org/careerseekers/userservice/controllers/TutorDocumentsController.kt
+++ src/main/kotlin/org/careerseekers/userservice/controllers/TutorDocumentsController.kt
@@ -7,7 +7,6 @@ import org.careerseekers.userservice.dto.docs.UpdateTutorDocsDto
 import org.careerseekers.userservice.entities.TutorDocuments
 import org.careerseekers.userservice.io.BasicSuccessfulResponse
 import org.careerseekers.userservice.io.converters.extensions.toHttpResponse
-import org.careerseekers.userservice.io.converters.extensions.toLongOrThrow
 import org.careerseekers.userservice.services.TutorDocumentsService
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -15,10 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("users-service/v1/tutor-docs")
@@ -37,38 +35,12 @@ class TutorDocumentsController(
     fun getDocsByUserId(@PathVariable id: Long) = service.getDocsByUserId(id)
 
     @PostMapping("/")
-    fun create(
-        @RequestPart("userId") userId: String,
-        @RequestPart("institution") institution: String,
-        @RequestPart("post") post: String,
-        @RequestPart("consentToTutorPdp") consentToTutorPdp: MultipartFile,
-    ): BasicSuccessfulResponse<TutorDocuments> {
-        val body = CreateTutorDocsDto(
-            userId = userId.toLongOrThrow(),
-            institution = institution,
-            post = post,
-            consentToTutorPdp = consentToTutorPdp,
-        )
-
-        return service.create(body).toHttpResponse()
-    }
+    fun create(@RequestBody item: CreateTutorDocsDto): BasicSuccessfulResponse<TutorDocuments> =
+        service.create(item).toHttpResponse()
 
     @PatchMapping("/")
-    fun update(
-        @RequestPart("id") id: String,
-        @RequestPart("institution", required = false) institution: String?,
-        @RequestPart("post", required = false) post: String?,
-        @RequestPart("consentToTutorPdp", required = false) consentToTutorPdp: MultipartFile,
-    ): BasicSuccessfulResponse<String> {
-        val body = UpdateTutorDocsDto(
-            id = id.toLongOrThrow(),
-            institution = institution,
-            post = post,
-            consentToTutorPdp = consentToTutorPdp
-        )
-
-        return service.update(body).toHttpResponse()
-    }
+    fun update(@RequestBody item: UpdateTutorDocsDto): BasicSuccessfulResponse<String> =
+        service.update(item).toHttpResponse()
 
     @DeleteMapping("/{id}")
     override fun deleteById(@PathVariable id: Long) = service.deleteById(id).toHttpResponse()
