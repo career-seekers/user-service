042ec797ac1a5f20fdf68691802fba2c756dbdf2
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-10-01 01:26:30 +0300

    feat(utils): created links biscuit generator

---
+++ src/main/kotlin/org/careerseekers/userservice/utils/BiscuitGenerator.kt
@@ -0,0 +1,21 @@
+package org.careerseekers.userservice.utils
+
+/**
+ * Object for generating unique token strings ("biscuits") for public invitation links.
+ *
+ * The main functionality is to create a random alphanumeric string consisting of
+ * uppercase letters, lowercase letters, and digits.
+ *
+ * @property length The length of the generated token string, default is 16 characters.
+ *
+ * @return A randomly generated alphanumeric string of the specified length.
+ */
+object BiscuitGenerator {
+
+    fun generateLinkBiscuit(length: Int = 16): String {
+        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+        return (1..length)
+            .map { allowedChars.random() }
+            .joinToString("")
+    }
+}
