b69b907924157c029b39b0b6796b7bcfe99fa692
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-10-04 02:34:06 +0300

    fix(service): create child docs fix service

---
+++ src/main/kotlin/org/careerseekers/userservice/services/ChildDocsFixService.kt
@@ -0,0 +1,39 @@
+package org.careerseekers.userservice.services
+
+import jakarta.transaction.Transactional
+import org.careerseekers.userservice.enums.DirectionAgeCategory
+import org.careerseekers.userservice.io.converters.convertDateToLocalDate
+import org.careerseekers.userservice.repositories.ChildDocsRepository
+import org.careerseekers.userservice.utils.AgeCalculator.calculateAge
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@Service
+class ChildDocsFixService(
+    private val childDocumentsService: ChildDocumentsService,
+    private val childDocsRepository: ChildDocsRepository
+) {
+
+    @Transactional
+    fun fixChildDocs() {
+        childDocumentsService.getAll()
+            .filter { it.learningClass == 0.toShort() }
+            .forEach { docs ->
+                val child = docs.child
+                val normalizedAgeCategory = DirectionAgeCategory.getAgeCategory(
+                    calculateAge(
+                        convertDateToLocalDate(child.dateOfBirth),
+                        LocalDate.of(2025, 12, 7)
+                    ), docs.learningClass
+                )
+
+                if (docs.ageCategory != normalizedAgeCategory) {
+                    docs.ageCategory = normalizedAgeCategory
+                    childDocsRepository.save(docs)
+
+                    println(Pair("Child: ${docs.child.id}", "Docs: ${docs.id}"))
+                }
+            }
+
+    }
+}
