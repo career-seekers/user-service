1e5b705ad8e8bec63de43b69974fb370a6f23aef
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-10-07 13:46:41 +0300

    fix(service): set minimal age to SCHOOL_1 to 6 years

--- src/main/kotlin/org/careerseekers/userservice/enums/DirectionAgeCategory.kt
+++ src/main/kotlin/org/careerseekers/userservice/enums/DirectionAgeCategory.kt
@@ -5,7 +5,7 @@ import org.careerseekers.userservice.exceptions.BadRequestException
 enum class DirectionAgeCategory(private val ageAlias: String) {
     PRESCHOOL_1("4-5 лет"),
     PRESCHOOL_2("6-7 лет"),
-    SCHOOL_1("7-9 лет"),
+    SCHOOL_1("6-8 лет"),
     SCHOOL_2("9-11 лет"),
     SCHOOL_3("12-13 лет");
 
@@ -14,7 +14,7 @@ enum class DirectionAgeCategory(private val ageAlias: String) {
             return when (age) {
                 in 4..5 -> PRESCHOOL_1
                 in 6..7 if learningClass == 0.toShort() -> PRESCHOOL_2
-                in 7..8 if learningClass != 0.toShort() -> SCHOOL_1
+                in 6..8 if learningClass != 0.toShort() -> SCHOOL_1
                 in 9..11 -> SCHOOL_2
                 in 12..13 -> SCHOOL_3
                 else -> throw BadRequestException("Данный возраст не поддерживается.")
