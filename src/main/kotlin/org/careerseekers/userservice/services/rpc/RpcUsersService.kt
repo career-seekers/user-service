3fc858be8c377dcdb9cadfea4df9cda65a7a1be1
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-10-17 07:31:32 +0300

    refactor(rpc): updated rpc service logic

--- src/main/kotlin/org/careerseekers/userservice/services/rpc/RpcUsersService.kt
+++ src/main/kotlin/org/careerseekers/userservice/services/rpc/RpcUsersService.kt
@@ -1,22 +1,23 @@
 package org.careerseekers.userservice.services.rpc
 
-import com.careerseekers.grpc.users.Child
-import com.careerseekers.grpc.users.ChildList
+import com.careerseekers.grpc.users.ChildId
+import com.careerseekers.grpc.users.ChildWithUser
 import com.careerseekers.grpc.users.User
 import com.careerseekers.grpc.users.UserId
-import com.careerseekers.grpc.users.UserWithChildren
 import com.careerseekers.grpc.users.UsersServiceGrpc
 import io.grpc.stub.StreamObserver
 import net.devh.boot.grpc.server.service.GrpcService
 import org.careerseekers.userservice.cache.UsersCacheLoader
 import org.careerseekers.userservice.io.converters.extensions.toCache
 import org.careerseekers.userservice.io.converters.extensions.toTimestamp
+import org.careerseekers.userservice.services.ChildService
 import org.careerseekers.userservice.services.UsersService
 import org.springframework.transaction.annotation.Transactional
 
 @GrpcService
 class RpcUsersService(
     private val usersService: UsersService,
+    private val childService: ChildService,
     private val usersCacheLoader: UsersCacheLoader,
 ) : UsersServiceGrpc.UsersServiceImplBase() {
 
@@ -48,27 +49,13 @@ class RpcUsersService(
     }
 
     @Transactional
-    override fun getUserWithChildren(request: UserId, responseObserver: StreamObserver<UserWithChildren>) {
-        val response = usersService.getById(
+    override fun getChildWithUser(request: ChildId, responseObserver: StreamObserver<ChildWithUser>) {
+        val response = childService.getById(
             request.id,
-            message = "Пользователь с ID ${request.id} не найден."
-        )!!.let { user ->
-            usersCacheLoader.loadItemToCache(user.toCache())
-
-            val childList = ChildList.newBuilder()
-            user.children?.forEach { child ->
-                val rpcChild = Child.newBuilder()
-                    .setId(child.id)
-                    .setLastName(child.lastName)
-                    .setFirstName(child.firstName)
-                    .setPatronymic(child.patronymic)
-                    .setDateOfBirth(child.dateOfBirth.toTimestamp())
-                    .build()
-
-                childList.addChildren(rpcChild)
-            }
-
-            UserWithChildren.newBuilder()
+            message = "Ребёнок с ID ${request.id} не найден."
+        )!!.let { child ->
+            val user = child.user
+            val rpcUser = User.newBuilder()
                 .setId(user.id)
                 .setFirstName(user.firstName)
                 .setLastName(user.lastName)
@@ -81,7 +68,15 @@ class RpcUsersService(
                 .setAvatarId(user.avatarId)
                 .setVerified(user.verified)
                 .setIsMentor(user.isMentor)
-                .setChildren(childList)
+                .build()
+
+            ChildWithUser.newBuilder()
+                .setId(child.id)
+                .setLastName(child.lastName)
+                .setFirstName(child.firstName)
+                .setPatronymic(child.patronymic)
+                .setDateOfBirth(child.dateOfBirth.toTimestamp())
+                .setUser(rpcUser)
                 .build()
         }
 
