71ea652405c2b1681e6b5563fc5289fe47830a8e
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-10-18 23:12:01 +0300

    refactor(annotations): updated utility annotation

--- src/main/kotlin/org/careerseekers/userservice/utils/Utility.kt
+++ src/main/kotlin/org/careerseekers/userservice/utils/Utility.kt
@@ -1,6 +1,13 @@
 package org.careerseekers.userservice.utils
 
+import org.springframework.core.annotation.AliasFor
 import org.springframework.stereotype.Component
 
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
 @Component
-annotation class Utility()
+annotation class Utility(
+    @get:AliasFor(annotation = Component::class, attribute = "value")
+    val value: String = ""
+)
