3c9e8f155c8a9605c690c62ab8254528c783debe
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-11-06 18:15:41 +0300

    feat(builders): add methods overloading with ready-made entities

--- src/main/kotlin/org/careerseekers/userservice/utils/builders/RpcUserBuilder.kt
+++ src/main/kotlin/org/careerseekers/userservice/utils/builders/RpcUserBuilder.kt
@@ -3,6 +3,7 @@ package org.careerseekers.userservice.utils.builders
 import com.careerseekers.grpc.users.User
 import org.careerseekers.userservice.annotations.Utility
 import org.careerseekers.userservice.cache.UsersCacheLoader
+import org.careerseekers.userservice.entities.Users
 import org.careerseekers.userservice.io.converters.extensions.toCache
 import org.careerseekers.userservice.io.converters.extensions.toTimestamp
 import org.careerseekers.userservice.services.UsersService
@@ -34,5 +35,25 @@ class RpcUserBuilder(
                 .setTgLink(user.telegramLink?.tgLink.toString())
                 .build()
         }
+    }
+
+    fun buildRpcUser(user: Users): User {
+        usersCacheLoader.loadItemToCache(user.toCache())
+
+        return User.newBuilder()
+            .setId(user.id)
+            .setFirstName(user.firstName)
+            .setLastName(user.lastName)
+            .setPatronymic(user.patronymic)
+            .setDateOfBirth(user.dateOfBirth?.toTimestamp())
+            .setEmail(user.email)
+            .setMobileNumber(user.mobileNumber)
+            .setPassword(user.password)
+            .setRole(user.role.toString())
+            .setAvatarId(user.avatarId)
+            .setVerified(user.verified)
+            .setIsMentor(user.isMentor)
+            .setTgLink(user.telegramLink?.tgLink.toString())
+            .build()
     }
 }
