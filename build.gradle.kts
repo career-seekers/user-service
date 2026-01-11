e50a8ca386ed4b3c2ae2e4ebf2ca3eb90ed059e9
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-01-11 05:32:54 +0300

    refactor(core): dependency versions moved to external .properties file

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,20 @@ repositories {
     mavenCentral()
 }
 
+val websocketVersion: String by project
+val graphqlVersion: String by project
+val jsonwebtokenVersion: String by project
+val jaxbApiVersion: String by project
+val mapstructVersion: String by project
+val grpcMessagingVersion: String by project
+val protobufVersion: String by project
+val grpcProtobufVersion: String by project
+val kotlinxSerializationVersion: String by project
+val kotlinxCoroutinesVersion: String by project
+val dotenvSpringVersion: String by project
+val javaxAnnotationVersion: String by project
+val mockkVersion: String by project
+
 dependencies {
     // Spring
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -49,16 +63,16 @@ dependencies {
 
     // Spring WebSocket
     implementation("org.springframework.boot:spring-boot-starter-websocket")
-    implementation("org.webjars:stomp-websocket:2.3.3")
+    implementation("org.webjars:stomp-websocket:$websocketVersion")
 
     // Spring GraphQL
     implementation("org.springframework.boot:spring-boot-starter-graphql")
-    implementation("com.netflix.graphql.dgs.codegen:graphql-dgs-codegen-gradle:8.3.0")
+    implementation("com.netflix.graphql.dgs.codegen:graphql-dgs-codegen-gradle:$graphqlVersion")
     testImplementation("org.springframework.graphql:spring-graphql-test")
 
     // JWT Auth
-    implementation("io.jsonwebtoken:jjwt:0.12.6")
-    implementation("javax.xml.bind:jaxb-api:2.3.0")
+    implementation("io.jsonwebtoken:jjwt:$jsonwebtokenVersion")
+    implementation("javax.xml.bind:jaxb-api:$jaxbApiVersion")
 
     // Databases
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -72,19 +86,19 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // Mapper
-    implementation("org.mapstruct:mapstruct:1.5.3.Final")
-    kapt("org.mapstruct:mapstruct-processor:1.5.3.Final")
+    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+    kapt("org.mapstruct:mapstruct-processor:$mapstructVersion")
 
     // Kafka messaging
     implementation("org.springframework.kafka:spring-kafka")
     testImplementation("org.springframework.kafka:spring-kafka-test")
 
     // gRPC messaging
-    implementation("net.devh:grpc-server-spring-boot-starter:3.1.0.RELEASE")
-    implementation("net.devh:grpc-client-spring-boot-starter:3.1.0.RELEASE")
-    implementation("com.google.protobuf:protobuf-java:4.28.2")
-    implementation("io.grpc:grpc-protobuf:1.57.2")
-    implementation("io.grpc:grpc-stub:1.57.2")
+    implementation("net.devh:grpc-server-spring-boot-starter:$grpcMessagingVersion")
+    implementation("net.devh:grpc-client-spring-boot-starter:$grpcMessagingVersion")
+    implementation("com.google.protobuf:protobuf-java:$protobufVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcProtobufVersion")
+    implementation("io.grpc:grpc-stub:$grpcProtobufVersion")
 
     //Kotlinx coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
@@ -93,20 +107,20 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 
     //Kotlinx serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$kotlinxSerializationVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
 
     // Utilities
-    implementation("one.stayfocused.spring:dotenv-spring-boot:1.0.0")
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("one.stayfocused.spring:dotenv-spring-boot:$dotenvSpringVersion")
+    implementation("javax.annotation:javax.annotation-api:$javaxAnnotationVersion")
     implementation("org.aspectj:aspectjweaver")
 
     // Tests
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("io.mockk:mockk-agent-jvm:1.13.7")
-    testImplementation("io.mockk:mockk:1.13.7")
+    testImplementation("io.mockk:mockk-agent-jvm:$mockkVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
 
     // Metrics
     implementation("io.micrometer:micrometer-tracing-bridge-brave")
