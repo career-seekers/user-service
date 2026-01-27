92a2a27b21f1e62f7bfdc5f2dccabd9afe163fec
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-01-27 20:30:43 +0300

    feat(grpc): created new getter for users by ids

--- src/main/kotlin/org/careerseekers/userservice/services/rpc/RpcUsersService.kt
+++ src/main/kotlin/org/careerseekers/userservice/services/rpc/RpcUsersService.kt
@@ -3,15 +3,30 @@ package org.careerseekers.userservice.services.rpc
 import com.careerseekers.grpc.users.*
 import io.grpc.stub.StreamObserver
 import net.devh.boot.grpc.server.service.GrpcService
+import org.careerseekers.userservice.io.converters.extensions.rpc.toList
+import org.careerseekers.userservice.io.converters.extensions.rpc.toRpcList
+import org.careerseekers.userservice.io.converters.extensions.rpc.toRpcUser
+import org.careerseekers.userservice.repositories.UsersRepository
 import org.careerseekers.userservice.utils.builders.RpcUserBuilder
 
 @GrpcService
 class RpcUsersService(
-    private val rpcUserBuilder: RpcUserBuilder
+    private val rpcUserBuilder: RpcUserBuilder,
+    private val usersRepository: UsersRepository,
 ) : UsersServiceGrpc.UsersServiceImplBase() {
 
     override fun getById(request: UserId, responseObserver: StreamObserver<User>) {
         val response = rpcUserBuilder.buildRpcUser(request.id)
+
+        responseObserver.onNext(response)
+        responseObserver.onCompleted()
+    }
+
+    override fun getUsersByIds(request: UserIds, responseObserver: StreamObserver<Users>) {
+        val response = usersRepository
+            .findAllById(request.toList())
+            .map { it.toRpcUser() }
+            .toRpcList()
 
         responseObserver.onNext(response)
         responseObserver.onCompleted()
